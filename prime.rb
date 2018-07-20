a1fae7ac92ac62ee5cad24af119e0a6910b3f22a
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 08:53:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
-def prime?
-  
+def prime?(num)
+  x = num.abs
+  if x == 1 || x == 0
+   return false 
+  else
+    (2...x).to_a.each do |num| 
+    return false if num % x == 0
+    end
+    true
+  end
 end
